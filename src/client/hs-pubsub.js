20e092d63e3ae8e584768ec07bdf0876f11d4ca5
Rei Colina <reinaldo13@gmail.com>
2015-12-10 07:04:03 -0800

    Initial skeleton

--- src/client/hs-pubsub.js
+++ src/client/hs-pubsub.js
@@ -1,1 +1,17 @@
+var hsps = {};
+(function () {
+  function HSPS() {
+    var self = this;
+    // utility private methods go here (below)
+    
+    // public api methods go here (below)
+    this.subscribe = function () {
+      // TODO
+    };
 
+    this.publish = function () {
+      // TODO
+    };
+  };
+  hsps = new HSPS();
+})();
